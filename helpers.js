97116bdacac1b2023f71c03213e997bdfb90ec68
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-02-17 20:49:05 -0500

    created helper utility for randomness in enumerable types

---
+++ helpers.js
@@ -0,0 +1,17 @@
+function scramble (word) {
+    const letters = word.split("");  // convert word into list of letters
+    const length = letters.length;
+
+    for(let i = length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const tmp = letters[i];
+        letters[i] = letters[j];
+        letters[j] = tmp;
+    }
+    return letters.join("");
+}
+
+function choice(items){
+    const index = Math.floor(Math.random() * items.length)
+    return items[index]
+}
